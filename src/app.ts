cc4784d344abbf88ff10ec7bbc13c099afa9696e
陳威愷 <96461229+Wiki4831@users.noreply.github.com>
2026-05-14 14:37:48 +0000

    chore: eslint error

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
   });
 
   const appVersion = String(process.env.APP_VERSION);
-  
+
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
